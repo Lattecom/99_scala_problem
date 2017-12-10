9a70c6102188143b18dce100bfc70bac9c0fc564
LatteCom <redeye217@gmail.com>
2017-12-11 00:29:59 +0900

    update comment

--- source/p01.scala
+++ source/p01.scala
@@ -1,9 +1,11 @@
 object P01 {
+  // pattern matching version
   def last[A](la: List[A]): A = la match {
     case h :: Nil => h 
     case h :: t => last(t)
   }
 
+  // foldLeft version
   def last_[A](la: List[A]): A = 
     la.foldRight(la.head)((acc, a) => if (a == la.head) acc else a)
 }
