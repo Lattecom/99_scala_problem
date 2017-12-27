0438b0a0ffd0efaf6a9fd71d44d83297422b063c
LatteCom <redeye217@gmail.com>
2017-12-27 20:04:05 +0900

    solve p02

---
+++ source/p02.scala
@@ -0,0 +1,15 @@
+object P02 {
+  // tailrec & pattern matching version
+  def length[A](la: List[A]): Int = {
+    @annotation.tailrec
+  	def go(l: List[A], acc: Int): Int = l match {
+  		case Nil => acc
+		  case h :: t => go(t, acc+1)
+  	}
+    go(la,0)
+  }
+
+  // foldLeft version
+  def length_[A](la: List[A]): Int = 
+    la.foldLeft(0)((acc, a) => acc + 1)
+}
