16a6cc2133cb6c002b0ce8a34ce4f45b4cd8fd1e
LatteCom <redeye217@gmail.com>
2018-04-13 13:27:02 +0900

    update p12

--- source/p12.scala
+++ source/p12.scala
@@ -12,19 +12,6 @@ res0: List[Symbol] = List('a, 'a, 'a, 'a, 'b, 'c, 'c, 'a, 'a, 'd, 'e, 'e, 'e, 'e
 */
 
 object P12 {
-  def encode[A](l: List[A]): List[(Int, A)] =
-    pack(l) map (a => (a.length, a.head))
-
-  def encode_2[A](l: List[A]): List[(Int, A)] =
-    l.foldRight(List[(Int, A)]())((a, z) => z match {
-      case h :: t => if(h._2 == a) (h._1 + 1, h._2) :: t else (1, a) :: z
-      case _ => (1, a) :: z
-    })
-
-  // P09
-  def pack[A](l: List[A]): List[List[A]] =
-    l.foldRight(Nil: List[List[A]])((a, z) => z match {
-      case h :: t => if (h.head == a) (a :: h) :: t else List(a) :: z
-      case _ => List(a) :: z
-    })
+  def decode[A](l: List[(Int, A)]): List[A] =
+    l.flatMap(a => List.fill(a._1)(a._2))
 }
