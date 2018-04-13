f523689bd3f469ec921742a2be3334642fbd92df
LatteCom <redeye217@gmail.com>
2018-04-13 13:17:09 +0900

    rename object name

--- source/p11.scala
+++ source/p11.scala
@@ -10,7 +10,7 @@ res0: List[Any] = List((4,'a), 'b, (2,'c), (2,'a), 'd, (4,'e))
 ```
 */
 
-object P10 {
+object P11 {
 
   def encodeModified[A](l: List[A]): List[Any] =
     encode(l) map (a => if (a._1 == 1) a._2 else a)
