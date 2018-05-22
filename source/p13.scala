0772e647fd654d58a9ac65d9dfa306bd659b38cc
LatteCom <redeye217@gmail.com>
2018-05-22 20:57:44 +0900

    update object name

--- source/p13.scala
+++ source/p13.scala
@@ -11,7 +11,7 @@ res0: List[(Int, Symbol)] = List((4,'a), (1,'b), (2,'c), (2,'a), (1,'d), (4,'e))
 ```
 */
 
-object P10 {
+object P13 {
   def encodeDirect[A](l: List[A]): List[(Int, A)] =
     l.foldRight(List[(Int, A)]())((a, z) => z match {
       case h :: t => if(h._2 == a) (h._1 + 1, h._2) :: t else (1, a) :: z
